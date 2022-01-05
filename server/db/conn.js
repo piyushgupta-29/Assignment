0dbf266c1cd49cacf74393a2f56acf7012c92569
Piyush Gupta <72931916+piyushgupta-29@users.noreply.github.com>
2022-01-05 09:44:43 +0530

    Update conn.js

--- server/db/conn.js
+++ server/db/conn.js
@@ -2,6 +2,9 @@ const mongoose = require('mongoose');
 
 const DB = process.env.DATABASE || "mongodb://localhost:27017/piyush";
 
-mongoose.connect(DB).then(() => {
+mongoose.connect(DB,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
     console.log(`connnection successful`);
 }).catch((err) => console.log(`no connection ${err}`));
