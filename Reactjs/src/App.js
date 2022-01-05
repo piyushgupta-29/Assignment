76a3f3e579412b6712abfa01f80bef8da9246818
Piyush Gupta <72931916+piyushgupta-29@users.noreply.github.com>
2022-01-05 20:28:38 +0530

    Update App.js

--- Reactjs/src/App.js
+++ Reactjs/src/App.js
@@ -5,10 +5,8 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import About from "./components/About";
-import Contact from "./components/Contact";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
-import ErrorPage from "./components/Errorpage";
 import Logout from "./components/Logout";
 
 import { initialState, reducer } from "./reducer/UseReducer";
@@ -17,21 +15,18 @@ import { initialState, reducer } from "./reducer/UseReducer";
 // we create a contextAPI 
 export const UserContext = createContext();
 
+  
+
 const Routing = () => {
   
   return (
     <>
-       <Switch>
       <Route exact path="/">
         <Home />
       </Route>
 
       <Route path="/about">
         <About />
-      </Route>
-
-      <Route path="/contact">
-        <Contact />
       </Route>
 
       <Route path="/login">
@@ -45,18 +40,13 @@ const Routing = () => {
       <Route path="/logout">
         <Logout />
       </Route>
-      
-      <Route>
-        <ErrorPage />
-      </Route>
-    </Switch>
     </>
   )
 }
 
 const App = () => {
 
-  //useReducer
+  //* we use useReducer
   const [state, dispatch] = useReducer(reducer, initialState);
 
   return (
